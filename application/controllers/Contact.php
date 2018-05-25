d86f6ed35fc228af628426e897c17e82ab824df6
somkriet <somkriet5769@gmail.com>
2018-05-25 17:37:00 +0700

    Contact  controllers

---
+++ application/controllers/Contact.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contact extends CI_Controller {
+
+	
+	public function __construct() {
+		
+		parent::__construct();
+		$this->load->library(array('session'));
+		$this->load->helper(array('url'));
+		$this->load->model('all_model');
+		
+	}
+
+	public function index()
+	{
+
+
+		// $this->session->set_userdata('login',$session_arr);
+		$data['user'] = $this->session->userdata('login');
+
+		// print_r($data['user']); exit();
+		$this->load->view('template/header');
+		$this->load->view('template/menu',$data);
+		$this->load->view('contact/contact_view',$data);
+		$this->load->view('template/footer');
+		
+	}
+}
